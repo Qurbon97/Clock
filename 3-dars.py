8217ff85943c2ac5b4829fec81edef71db9e8dd4
Qurbon97 <75775093+Qurbon97@users.noreply.github.com>
2023-12-20 22:00:48 +0500

    Added some lessons
    3,4

---
+++ 3-dars.py
@@ -0,0 +1,28 @@
+#3-dars
+#Amalyot
+print(" \"Nexia\", \"Tico\", \'Damas\' ko\'rganlar qilar havas")
+
+
+print(5**4)
+#5 ning 4-darajasini 625
+
+
+print(22%4)
+#22 ni 4 ga bo'lganda 2 qoldiq qoladi
+
+print(125*125)
+print(125*4)
+#Tomonlari 125 ga teng kvadratning yuzi 15625 va perimetri 500 ga teng
+
+print("S=pr2", "S=", 3.14*6**2)
+#Diametri 12 ga teng bo'lgan doiraning yuzi 113.04 
+
+print("c=", (6**2+7**2)**(1/2))
+#Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi 9.21 (Pifagor teoremasidan foydalaning)
+
+#-------------------Yozuv ko'rinishida chiqarish------------------
+print("1) 5 ning 4-darajasi", 5**4)
+print("2) 22 ni 4 ga bo'lganda", 22%4, "qoldiq qoladi")
+print("3) Tomonlari 125 ga teng kvadratning yuzi", 125*125, "va perimetri", 125*4, "ga teng")
+print("4) Diametri 12 ga teng bo'lgan doiraning yuzi", "S=", 3.14*6**2, "ga teng")
+print("5) Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", "c=", (6**2+7**2)**(1/2), "ga teng")
---
+++ 4-dars.py
@@ -0,0 +1,20 @@
+#4-dars
+
+
+Greeting = "Hello World !"
+print(Greeting)
+
+xabar = "Darsliklarni tezroq o'z. kerak"
+print(xabar)
+
+xabar = "Darslarni yaxshi o'zlashtiramiz"
+print(xabar) # O'zgaruvchi qiymati o'zgardi.
+
+# Pastdagi kod xato Chunki Python key word i ishlatilgan !
+# class = "Xato baribir"
+# print(class)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi", radius, "ga teng aylananing yuzi=", aylana_yuzi)
